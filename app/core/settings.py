c3cc2b8006e199b0e1dd5941156bc3536ae49dca
Garvis123 <parvmodi11@gmail.com>
2026-04-30 18:24:55 +0530

    feat: add db connection function

--- app/core/settings.py
+++ app/core/settings.py
@@ -7,11 +7,11 @@ class Settings(BaseSettings):
     APP_DESCRIPTION = "A simple chat application"
 
     # DATABASE CONFIG
-    HOST = os.getenv("DB_HOST")
-    PORT = os.getenv("DB_PORT")
-    PASSWORD = os.getenv("DB_PASSWORD")
-    NAME = os.getenv("DB_NAME")
-    USER = os.getenv("DB_USER")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
 
     # jwt config
     SECRET_KEY= os.getenv("SECRET_KEY")
--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,31 @@
 import psycopg2
 import psycopg2.extras
 from contextlib import contextmanager
-import os
+from core.settings import settings
+
+db_config = {
+    "host": settings.DB_HOST,
+    "port":settings.DB_PORT,
+    "user":settings.DB_USER,
+    "password":settings.DB_PASSWORD,
+    "database":settings.DB_NAME
+}
+
+# connection
+def get_connection():
+    return psycopg2.connect(**db_config)
+
+# connection
+@contextmanager
+def get_db_connection():
+    conn = get_connection()
+    try:
+        yield conn
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
 
 
